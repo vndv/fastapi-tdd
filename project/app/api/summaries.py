395c27e9dcf65d0e02e801c8d63d949b61b8f7fe
Ivan Matveev <skioneim@gmail.com>
2024-10-09 21:45:25 +0300

    feat: add linter and formater

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,10 +1,10 @@
 from typing import List
+
 from fastapi import APIRouter
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 from app.models.tortoise import SummarySchema
-
 
 router = APIRouter()
 
--- project/app/config.py
+++ project/app/config.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
     testing: bool = bool(0)
     database_url: AnyUrl = None
 
+
 @lru_cache
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
