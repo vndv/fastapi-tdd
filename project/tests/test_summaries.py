36b0dd8f70d0590eb0ba1c22c50e850d5f0ff727
Ivan Matveev <skioneim@gmail.com>
2024-10-10 20:07:33 +0300

    feat: update tests

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -7,7 +9,7 @@ def test_create_summary(test_app_with_db):
     )
 
     assert response.status_code == 201
-    assert response.json()["url"] == "https://foo.bar"
+    assert response.json()["url"] == "https://foo.bar/"
 
 
 def test_create_summaries_invalid_json(test_app):
@@ -25,6 +27,12 @@ def test_create_summaries_invalid_json(test_app):
         ]
     }
 
+    response = test_app.post("/summaries/", data=json.dumps({"url": "invalid://url"}))
+    assert response.status_code == 422
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
+
 
 def test_read_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -37,7 +45,7 @@ def test_read_summary(test_app_with_db):
 
     response_dict = response.json()
     assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["url"] == "https://foo.bar/"
     assert response_dict["summary"]
     assert response_dict["created_at"]
 
@@ -46,6 +54,21 @@ def test_read_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.get("/summaries/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
+
+    response = test_app_with_db.get("/summaries/0/")
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "ctx": {"gt": 0},
+                "input": "0",
+                "loc": ["path", "id"],
+                "msg": "Input should be greater than 0",
+                "type": "greater_than",
+                "url": "https://errors.pydantic.dev/2.9/v/greater_than",
+            }
+        ]
+    }
 
 
 def test_read_all_summaries(test_app_with_db):
@@ -59,3 +82,140 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+
+
+def test_remove_summaries(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+
+    assert response.status_code == 200
+    assert response.json() == {"id": summary_id, "url": "https://foo.bar/"}
+
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+    response = test_app_with_db.delete("/summaries/0/")
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "ctx": {"gt": 0},
+                "input": "0",
+                "loc": ["path", "id"],
+                "msg": "Input should be greater than 0",
+                "type": "greater_than",
+                "url": "https://errors.pydantic.dev/2.9/v/greater_than",
+            }
+        ]
+    }
+
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
+    )
+
+    assert response.status_code == 200
+
+    response_dict = response.json()
+
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar/"
+    assert response_dict["summary"] == "updated!"
+    assert response_dict["created_at"]
+
+
+@pytest.mark.parametrize(
+    "summary_id, payload, status_code, detail",
+    [
+        [
+            999,
+            {"url": "https://foo.bar", "summary": "updated!"},
+            404,
+            "Summary not found",
+        ],
+        [
+            0,
+            {"url": "https://foo.bar", "summary": "updated!"},
+            422,
+            [
+                {
+                    "type": "greater_than",
+                    "loc": ["path", "id"],
+                    "msg": "Input should be greater than 0",
+                    "input": "0",
+                    "ctx": {"gt": 0},
+                    "url": "https://errors.pydantic.dev/2.9/v/greater_than",
+                }
+            ],
+        ],
+        [
+            1,
+            {},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "url"],
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.9/v/missing",
+                },
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.9/v/missing",
+                },
+            ],
+        ],
+        [
+            1,
+            {"url": "https://foo.bar"},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {"url": "https://foo.bar"},
+                    "url": "https://errors.pydantic.dev/2.9/v/missing",
+                }
+            ],
+        ],
+    ],
+)
+def test_update_summary_invalid(
+    test_app_with_db, summary_id, payload, status_code, detail
+):
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/", data=json.dumps(payload)
+    )
+    assert response.status_code == status_code
+    print(response.json()["detail"])
+    assert response.json()["detail"] == detail
+
+
+def test_update_summary_invalid_url(test_app):
+    response = test_app.put(
+        "/summaries/1/",
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
+    )
+    assert response.status_code == 422
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
